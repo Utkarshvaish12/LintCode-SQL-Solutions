a4a8da30d0a1558df9b76d0c7bf54291b704eba0
Utkarsh Vaish <122693972+Utkarshvaish12@users.noreply.github.com>
2023-09-22 01:04:19 +0530

    Data Median.sql

--- Medium/Data Median.sql
+++ Medium/Data Median.sql
@@ -1,1 +1,27 @@
+WITH M AS (
+  SELECT MAX(Row_Num) AS mr
+  FROM (
+    SELECT ROW_NUMBER() OVER (ORDER BY num) AS Row_Num
+    FROM Number
+  ) AS RankedData
+),
+RankedData AS (
+  SELECT
+    ROW_NUMBER() OVER (ORDER BY num) AS Row_Num,
+    num
+  FROM Number
+)
 
+SELECT
+ ( CASE
+    WHEN M.mr % 2 = 0 THEN
+      (SELECT AVG(num)
+       FROM RankedData
+       WHERE Row_Num IN (M.mr / 2, M.mr / 2 + 1))
+    WHEN M.mr % 2 != 0 THEN
+      (SELECT num
+       FROM RankedData
+       WHERE Row_Num = (M.mr + 1) / 2)
+  END) AS median
+FROM RankedData,M
+LIMIT 1;
